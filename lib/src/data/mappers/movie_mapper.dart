5b9b0ffd47f907d76b94d4a2b8ea1407ddb53bdc
Julian Buitrago <julianbuitrago@MacBook-Pro-de-Julian.local>
2021-09-20 22:33:35 -0500

    refactor(data): update movie mapper implementation

--- lib/src/data/mappers/movie_mapper.dart
+++ lib/src/data/mappers/movie_mapper.dart
@@ -2,31 +2,17 @@ import 'package:flutter_movies_app/src/domain/entities/movie.dart';
 
 const imageUrl = 'http://image.tmdb.org/t/p/w300';
 
-class MovieMapper extends Movie {
-  MovieMapper({
-    required id,
-    required title,
-    required backdropImage,
-    posterImage,
-    releaseDate,
-    overview,
-  }) : super(
-          id: id,
-          title: title,
-          backdropImage: backdropImage,
-          posterImage: posterImage,
-          releaseDate: releaseDate,
-          overview: overview,
-        );
+class MovieMapper {
+  MovieMapper() : super();
 
-  factory MovieMapper.fromJson(dynamic json) {
+  static Movie fromJson(dynamic json) {
     DateTime? date;
 
     if (json['release_date'] != null) {
       date = DateTime.parse(json['release_date']);
     }
 
-    return MovieMapper(
+    return Movie(
       id: json['id'],
       title: json['title'] ?? json['original_title'] ?? json['original_name'],
       backdropImage: '$imageUrl${json["backdrop_path"]}',
